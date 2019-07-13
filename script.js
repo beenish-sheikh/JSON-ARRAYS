f6a15498bdd117f1652cbb236eefbfcd06e0707a
Beenish-sheikh <50364249+beenish-sheikh@users.noreply.github.com>
2019-07-13 14:06:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,267 @@
+let hotel = {
+
+    hotel: "pc luxery hotel karachi",
+    address: "karachi 17 kilometers from the Jinnah International Airport",
+    phone: "098764321",
+    email: "abc12345@gmail.com",
+
+    ceo: [{
+        name: 'm salam',
+        position: "ceo",
+        education: "masters in public relations",
+        contect: 09876543,
+        email: "werty@gmail.com",
+        address: "24 deence"
+    }],
+
+    staff: [{
+        managers: [{
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+            },
+
+        ],
+
+        accountants: [{
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+
+
+
+        ],
+
+        receptionists: [{
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+        ],
+
+        recreationalFesilities: ["Temperature controlled indoor swimming pool, jacuzzi, sauna, fully equipped gym. "
+
+        ],
+
+        wiaters: [{
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+            },
+
+        ],
+
+        sweepers: [{
+            name: "m shahid memon",
+            position: "manager",
+            education: "masters in public relations",
+            contect: 09876543,
+            email: "werty@gmail.com",
+            address: "gulshan town"
+
+        }],
+
+        guards: [{
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            },
+            {
+                name: "m shahid memon",
+                position: "manager",
+                education: "masters in public relations",
+                contect: 09876543,
+                email: "werty@gmail.com",
+                address: "gulshan town"
+
+            }
+        ],
+
+
+        chefs: [{
+            name: "m shahid memon",
+            position: "manager",
+            education: "masters in public relations",
+            contect: 09876543,
+            email: "werty@gmail.com",
+            address: "gulshan town"
+
+        }],
+
+
+
+    }],
+
+    healthClubs: [
+        "The hotel provides easy access to the main commercial,",
+        " banking and shopping districts and consulates among other essential places in the city.",
+        " Complimentary airport transfers through shuttle service are also available on prior request."
+    ],
+
+    freeWifi: [
+        "Our 24-hour concierge desk and round-the-clock front desk will help you explore",
+        " the area and make reservations for you at any of the hotel’s restaurants. ",
+        "Where you plan to travel in the city, we can guide you to your destination."
+    ],
+
+
+    bussinessCenters: [
+        "Overlooking the sprawling city, Pearl-Continental Hotel Karachi ",
+        "is a part of the most significant chain of hotels in Pakistan.",
+        " The Hotel is perfect for both business and leisure travellers.",
+        "Guests can choose from a variety of rooms and suites to enjoy their stay at the five-star hotel."
+    ],
+
+    swimmingPools: [
+        "green,blue,red,yello,purple"
+    ],
+
+    prakingArea: [
+        "1stfloor",
+        "2nd floor",
+        "3rd floor",
+        "basement"
+    ],
+    rooms: [
+        "deliuxe",
+        "executive",
+        "pearl"
+    ],
+    dinning: [
+        "marcopolo",
+        "tai pan",
+        "chandi",
+        "sakura",
+        "JSON'S STEAK HOUSE",
+        "grape wine cafe",
+        "executive lounge"
+
+    ],
+    avFesilities: [
+        "Video Projection System",
+        "Screens",
+        "Integrated P/A System",
+        "Data Ports",
+        "Audio recording equipment",
+        "Magic board",
+        "Flip chart",
+        "Laser printer",
+        "Overhead projectors",
+        "Slide projector",
+    ],
+
+    meetings: [
+        "Express capsule elevators exclusively for roof top Board Rooms,",
+        " Executive Floors, “Pearl Lady” Program, Steak House, Chandni and Sakura Restaurants.",
+        " Shopping arcade, express check-in, "
+    ],
+
+    events: [
+        "Extensive banqueting ideal for weddings and events in Karachi and seminar/convention",
+        " space from boardroom style of 8 to a capacity of 1500 people. Ball Room, Shalimar,",
+        " Dilkusha, Henna, Kohinoor, Mahnoor, Emerald and Zaver and Marquee. Tulip, Carnation ",
+        " Orchid panoramic board Rooms."
+
+    ],
+    offers: [
+        "Pearl Continental Hotel Karachi presents a range of special offers and deals for any purpose: ",
+        " business, leisure, dining…Choose the offer that best suits you and enjoy all the luxuries Karachi",
+        " has to offer at the best price."
+    ]
+
+
+
+
+
+
+
+
+}
